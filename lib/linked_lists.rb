3d6e61e2fac86f1ba99c7ead456bb94d9d73ab84
Darin <darinchengg@gmail.com>
2022-07-11 14:09:10 -0500

    Add #append, #prepend, and #size methods for LinkedList class

--- lib/linked_lists.rb
+++ lib/linked_lists.rb
@@ -1,14 +1,39 @@
 class LinkedList
   attr_accessor :name
+  attr_reader :head, :tail
 
   def initialize
-    @head = head
-    @tail = tail
+    @head = nil
+    @tail = nil
+  end
+
+  def append(data)
+    node = Node.new(data)
+    @head = node if @head.nil?
+    @tail.next = node unless @tail.nil?
+    @tail = node
+  end
+
+  def prepend(data)
+    node = Node.new(data)
+    @tail = node if @tail.nil?
+    node.next = @head unless @head.nil?
+    @head = node
+  end
+
+  def size
+    number_of_nodes = 0
+    node = @head
+    until node.nil?
+      number_of_nodes += 1
+      node = node.next
+    end
+    number_of_nodes
   end
 end
 
 class Node
-  attr_acessor :data, :next
+  attr_accessor :data, :next
 
   def initialize(data = nil)
     @data = data
